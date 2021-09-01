aaeb711f212c656dff6868f97717d05d1f6e5375
gjwehnes <gjwehnes@L829WD150008.edu.cbe.ab.ca>
2021-09-01 16:11:06 -0600

    added exercise 003-005

---
+++ src/ex003/MenuItem.java
@@ -0,0 +1,9 @@
+package ex003;
+
+public interface MenuItem {
+
+	String getName();
+	
+	double getPrice();
+	
+}
---
+++ src/ex005/TrueFalseQuestion.java
@@ -0,0 +1,9 @@
+package ex005;
+
+
+
+public class TrueFalseQuestion
+{
+// TODO: as a starting point, override the appropriate method(s) to present the question as specified
+
+}
